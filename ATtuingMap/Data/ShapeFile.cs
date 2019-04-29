b030fb0f5db2d6a23b569d731af61d4c78b8cc6d
huhy <1300776935@qq.com>
2019-04-29 11:34:31 +0800

    '初始化项目'

---
+++ ATtuingMap/Data/ShapeFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATtuingMap.Data
+{
+    /// <summary>
+    /// shp文件读取类
+    /// </summary>
+    public class ShapeFile
+    {
+        /// <summary>
+        /// 传入shp文件初始化类
+        /// </summary>
+        /// <param name="filename"></param>
+        public ShapeFile(string filename) {
+
+        }
+    }
+}
