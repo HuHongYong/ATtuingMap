b6e234855fd4067e5744402c2fabdddb5eae8e8b
huhy <1300776935qq@163.com>
2019-05-05 22:41:50 +0800

    '地图缩放完成'

--- ATtuingMap.DemoWinForm/Form1.Designer.cs
+++ ATtuingMap.DemoWinForm/Form1.Designer.cs
@@ -41,7 +41,9 @@
             this.pictureBox1.Size = new System.Drawing.Size(687, 430);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
             this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
             // 
             // label1
             // 
--- ATtuingMap.DemoWinForm/Form1.cs
+++ ATtuingMap.DemoWinForm/Form1.cs
@@ -13,7 +13,14 @@ namespace ATtuingMap.DemoWinForm
 {
     public partial class Form1 : Form
     {
+        //鼠标按住不放的坐标点
+        private System.Drawing.Point _mousedrag;
+        //map核心类
         private Map myMap;
+        //是否在进行拖放
+        private bool _mousedragging;
+        //按下鼠标当前地图的拷贝
+        private Image _mousedragImg;
         public Form1()
         {
             InitializeComponent();
@@ -21,7 +28,8 @@ namespace ATtuingMap.DemoWinForm
 
         private void Form1_Load(object sender, EventArgs e)
         {
-             myMap = new Map(pictureBox1.Size);
+
+            myMap = new Map(pictureBox1.Size);
             ///目前第一版只支持点
             ShapeFile shapeFile = new ShapeFile(@"GeoData\地级城市驻地.shp");
             VectorLayer layer = new VectorLayer(shapeFile);
@@ -33,13 +41,64 @@ namespace ATtuingMap.DemoWinForm
             myMap.Layers.Add(layer);
             myMap.Layers.Add(layer1);
             myMap.ZoomToExtents();
-            pictureBox1.Image= myMap.GetMap();
+            pictureBox1.Image = myMap.GetMap();
+            //鼠标样式
+            pictureBox1.Cursor = Cursors.Hand;
+            this.pictureBox1.MouseWheel += new MouseEventHandler(MapImage_Wheel);
         }
 
         private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
         {
-           var point= Transform.MapToWorld(new PointF(e.X,e.Y),myMap);
+            var point = Transform.MapToWorld(new PointF(e.X, e.Y), myMap);
             label1.Text = $"坐标X:{point.X}  Y:{point.Y}";
+            //拖动已有图像
+            if (_mousedragging)
+            {
+                Bitmap _dragImg1 = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                Graphics g = Graphics.FromImage(_dragImg1);
+                g.Clear(Color.Transparent);
+                g.DrawImageUnscaled(_mousedragImg,
+                                    new System.Drawing.Point(e.Location.X - _mousedrag.X,
+                                                             e.Location.Y - _mousedrag.Y));
+                g.Dispose();
+                pictureBox1.Image = _dragImg1;
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            System.Drawing.Point pnt = new System.Drawing.Point(pictureBox1.Width / 2 + (_mousedrag.X - e.Location.X),
+                                                   pictureBox1.Height / 2 + (_mousedrag.Y - e.Location.Y));
+            //修改鼠标拖动后的地图中心点
+            myMap.Center = Transform.MapToWorld(pnt, myMap);
+            pictureBox1.Image = myMap.GetMap();
+            _mousedragging = false;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            //鼠标按下的屏幕坐标位置
+            _mousedrag = e.Location;
+            _mousedragging = true;
+            _mousedragImg = pictureBox1.Image;
+        }
+        /// <summary>
+        /// 鼠标滚轮触发缩放地图事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MapImage_Wheel(object sender, MouseEventArgs e)
+        {
+            //重新定位中心点
+            myMap.Center = Transform.MapToWorld(new System.Drawing.Point(e.X, e.Y), myMap);
+            double scale = (e.Delta / 120.0);
+            double scaleBase = 1 + (2.0 / 10 );
+            myMap.Zoom *= Math.Pow(scaleBase, scale);
+            int NewCenterX = (pictureBox1.Width / 2) + ((pictureBox1.Width / 2) - e.X);
+            int NewCenterY = (pictureBox1.Height / 2) + ((pictureBox1.Height / 2) - e.Y);
+            //修改鼠标缩放后的地图中心点
+            myMap.Center = Transform.MapToWorld(new System.Drawing.Point(NewCenterX, NewCenterY), myMap);
+            pictureBox1.Image = myMap.GetMap();
         }
     }
 }
